375236e02e72c5ae50137dffc9d4915d021fb374
naxosit <128820710+naxosit@users.noreply.github.com>
2023-05-08 12:51:33 -0400

    Función valor equivalente
    Retorna la división entre el valor divisa, y el equivalente a 1 USD.

---
+++ fEquivalente.py
@@ -0,0 +1,16 @@
+from ingreso_datos import Divisa   
+
+# vDivisa = float
+#eqA1USD = float
+# vEquivalente: float float -> float
+
+def vEquivalente_eur(vDivisa, eqA1USD):
+    eqA1USD = 0.90567
+    return vDivisa / eqA1USD
+print(f"El valor equivalente de euro/s a dólar/es americano/s, es {vEquivalente_eur()}")
+
+def vEquivalente_lib(vDivisa, eqA1USD):
+    eqA1USD = 0.79043
+    return vDivisa / eqA1USD
+print(f"El valor equivalente de libra/s esterlina/s a dólar/es americano/s, es {vEquivalente_lib()}")
+
