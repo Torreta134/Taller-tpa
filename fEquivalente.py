fcc1fc5043143131ef3c8785bf202789ef2fe37c
naxosit <128820710+naxosit@users.noreply.github.com>
2023-05-08 13:00:57 -0400

    Update fEquivalente.py
    No era necesaria la variable eqA1USD, ya que se define posteriormente en la misma función.

--- fEquivalente.py
+++ fEquivalente.py
@@ -4,12 +4,12 @@ from ingreso_datos import Divisa
 #eqA1USD = float
 # vEquivalente: float float -> float
 
-def vEquivalente_eur(vDivisa, eqA1USD):
+def vEquivalente_eur(vDivisa):
     eqA1USD = 0.90567
     return vDivisa / eqA1USD
 print(f"El valor equivalente de euro/s a dólar/es americano/s, es {vEquivalente_eur()}")
 
-def vEquivalente_lib(vDivisa, eqA1USD):
+def vEquivalente_lib(vDivisa):
     eqA1USD = 0.79043
     return vDivisa / eqA1USD
 print(f"El valor equivalente de libra/s esterlina/s a dólar/es americano/s, es {vEquivalente_lib()}")
